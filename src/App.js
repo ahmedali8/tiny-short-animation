1cae4ae3f5ac12a012dcb0ea8d00980e7b600149
ahmedali8 <imahmedalibhatti@gmail.com>
2020-09-26 19:06:08 +0500

    added Router and Routes to the app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import React from 'react';
-import { Header, Home, Destinations, Plans, CoupleTour, Booking, AboutUs, ContactUs, Footer, NotFound } from './components';
+import { Routes, Route } from 'react-router-dom';
+import {
+    Header,
+    Home,
+    Destinations,
+    Plans,
+    CoupleTour,
+    Booking,
+    AboutUs,
+    ContactUs,
+    Footer,
+    NotFound
+} from './components';
 
 import styles from './App.module.css';
 
 function App() {
     return (
         <div className={styles.app}>
-            Hello World
+            <Header />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/destinations" element={<Destinations />} />
+                <Route path="/plans" element={<Plans />} />
+                <Route path="/coupletour" element={<CoupleTour />} />
+                <Route path="/booking" element={<Booking />} />
+                <Route path="/aboutus" element={<AboutUs />} />
+                <Route path="/contactus" element={<ContactUs />} />
+
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+            <Footer />
         </div>
     );
 }
